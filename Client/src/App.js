f9b235167948af553b5bd1af3d3c5592e7b644ab
zomgbabz <75215151+zomgbabz@users.noreply.github.com>
2023-01-23 15:17:36 -0700

    DB-FE-NewsfeedButtons

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SigninPage from "./components/Pages/SigninPage/SigninPage";
 import Homepage from "./components/Pages/Homepage/Homepage";
+import RegistrationPage from "./components/Pages/RegistrationPage/RegistrationPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SigninPage />} />
+        <Route path="/" element={<RegistrationPage />} />
         <Route path="/homepage" element={<Homepage />} />
       </Routes>
     </Router>
--- Client/src/components/NewsFeed/FeedCard.js
+++ Client/src/components/NewsFeed/FeedCard.js
@@ -1,10 +1,20 @@
+import React, { useState } from "react";
 import { Col } from "react-bootstrap";
 import "./App.css";
+import LikeButton from "../LikeButton";
+import CommentButton from "../CommentButton";
+import ShareButton from "../ShareButton";
 
 export const FeedCard = ({ link, title, description, imgUrl }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleCommentClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <Col size={1} sm={10} md={10}>
-      <div className="proj-imgbx" onClick={() => window.open({ link })}>
+      <div className="proj-imgbx">
         <img
           className="profile-picture"
           src={require("./blank-profile.PNG")}
@@ -17,6 +27,10 @@ export const FeedCard = ({ link, title, description, imgUrl }) => {
           <div className="description-textbox">
             <p>{description}</p>
           </div>
+          <LikeButton />
+          <CommentButton onCommentClick={handleCommentClick} />
+          {isExpanded && <input type="text" placeholder="Leave a comment..." />}
+          <ShareButton />
         </div>
       </div>
     </Col>
