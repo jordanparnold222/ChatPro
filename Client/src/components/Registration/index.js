728840462d879e4687be82db4600e3579b01b2d4
Jordan Arnold <42490695+jordanparnold222@users.noreply.github.com>
2023-01-26 22:42:25 -0700

    it aint workin'

--- Client/src/components/Registration/index.js
+++ Client/src/components/Registration/index.js
@@ -13,21 +13,88 @@ import {
   Text,
   NavBtn,
   NavBtnLink,
+  baseUrl,
 } from "./SigninElements";
 
-const SignIn = () => {
+
+
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      FirstName: '',
+      LastName: '',
+      Username: '',
+      Email: '',
+      Password: '',
+    };
+  }
+
+    //assigns user input to object attribute to be sent out
+    handleChange = (event) => {
+      const { name, value } = event.target;
+      this.setState({ [name]: value });
+    }
+
+
+
+    handleSubmit = (event) => {
+      event.preventDefault();
+      const user = {
+        FirstName: this.state.firstName,
+        LastName: this.state.lastName,
+        Username: this.state.username,
+        Email: this.state.email,
+        Password: this.state.password,
+      };
+      fetch(baseUrl + 'api/Users', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(user),
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log('Success:', data);
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+      });
+  }
+  
+  render() {
   return (
     <>
+
+
       <Container>
+        
         <FormWrap>
           <FormContent>
-            <Form action="#">
+            <Form action="#" onSubmit={this.handleSubmit}>
               <FormH1>Register your account</FormH1>
+
+              <FormLabel htmlFor="for">First Name</FormLabel>
+              <FormInput type="FirstName" required name="firstName"
+              value={this.state.firstName} onChange={this.handleChange}/>
+
+              <FormLabel htmlFor="for">Last Name</FormLabel>
+              <FormInput type="LastName" required name="lastName"
+              value={this.state.lastName} onChange={this.handleChange}/>
+
+              <FormLabel htmlFor="for">Username</FormLabel>
+              <FormInput type="Username" required name="username"
+              value={this.state.username} onChange={this.handleChange}/>
+
               <FormLabel htmlFor="for">Email</FormLabel>
-              <FormInput type="email" required />
+              <FormInput type="email" required name="email"
+              value={this.state.email} onChange={this.handleChange}/>
+
               <FormLabel htmlFor="for">Password</FormLabel>
-              <FormInput type="password" required />
-              <NavBtnLink to="/homepage" type="submit">
+              <FormInput type="password" required name="password"
+              value={this.state.password} onChange={this.handleChange}/>
+
+              <NavBtnLink type="submit">
                 Continue
               </NavBtnLink>
 
@@ -38,6 +105,7 @@ const SignIn = () => {
       </Container>
     </>
   );
+};
 };
 
 export default SignIn;
